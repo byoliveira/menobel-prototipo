eb71a2f817bb1b912059ce06d2b228c98524f820
Bruno Oliveira <49489698+byoliveira@users.noreply.github.com>
2024-10-07 18:18:15 -0300

    implementacoes de novos modulos do prototipo

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const hamburguerMenu = document.querySelector('.hamburguer-menu');
-    const userProfile = document.querySelector('.user-profile');
-
-    hamburguerMenu.addEventListener('click', () => {
-        userProfile.classList.toggle('show-dropdown');
-    });
-});
